aafc1e8762e9678f868ba779e6e58380a4448a6f
Jesse Peterson <jessep473@gmail.com>
2018-10-17 12:55:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ function theBeatlesPlay(mus, inst) {
 }
 
 function johnLennonFacts(facts) {
-  
+  facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 }
 
 
